b4cfc289702f3c5c33814aec1ca92492fb944656
Fabrizio Tedeschi <39946014+Frizio03@users.noreply.github.com>
2019-10-24 16:23:07 +0200

    Update Voti.cpp

--- Voti.cpp
+++ Voti.cpp
@@ -1,5 +1,11 @@
 // Autore: Fabrizio Tedeschi
 
+/*
+il programma crea un vettore di 15 elementi random
+il vettore viene riordinato
+si cerca uno specifico numero dentro al vettore
+*/
+
 // libreria principale
 #include <iostream>
 //librerie random
@@ -14,6 +20,7 @@ int random(int n1, int n2)
 	return rand() % (n1-n2+1)+n1;
 }
 
+//funzione scambia
 int scambia(int &a, int &b)
 {
 	int box = a;
@@ -21,7 +28,7 @@ int scambia(int &a, int &b)
 	b = box;
 }
 
-//ordinamento ingenuodi un vettore
+//ordinamento ingenuo di un vettore
 int ordinamento_ingenuo(int v[], int len)
 {
 	for (int i=0; i<len-1; i++)
@@ -33,6 +40,7 @@ int ordinamento_ingenuo(int v[], int len)
 		}
 	}
 	
+	cout << "vettore random: " << endl;
 	for (int i=0; i<len; i++)
 	{
 		cout << v[i] << " / ";
@@ -43,6 +51,7 @@ int ordinamento_ingenuo(int v[], int len)
 //creazione di un vettore con elementi random
 int rand_vett(int numero)
 {
+	cout << "vettore ordinato:" << endl;
 	int vettore[numero];
 	cout << "lista:" << endl;
 	for (int i=0; i<numero; i++)
@@ -60,5 +69,6 @@ int main()
 	//inizializzazione dei millisecondi
 	srand(time (NULL));
 	
+	//si crea e si ordina un  vettore di x elementi
 	rand_vett(15);
 }
