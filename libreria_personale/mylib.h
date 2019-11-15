43bbb6bac5c5dbf5f6d0b0baede957cbccddce5e
Frizio03 <fabrizio.tedeschi.scuola@gmail.com>
2019-11-15 15:37:48 +0100

    libreria personale vettori

--- libreria_personale/mylib.h
+++ libreria_personale/mylib.h
@@ -1,103 +1,145 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-int raddoppia(int n)
-{
-	return n*2;
-}
-
 //funzione random int
-int random(int n1, int n2)
+int random_int(int n1, int n2)
 {
 	return rand() % (n1-n2+1)+n1;
 }
 
-//funzione per scambiare 2 variabili
-int scambia(int &a, int &b)
+//funzione random float
+float random_float(int n1, int n2)
+{
+	int v1 = rand() % (n1-n2+1)+n1;
+	return (float)v1/10;
+}
+
+//funzione per scambiare 2 variabili int
+void scambia_int(int &a, int &b)
 {
 	int box = a;
 	a = b;
 	b = box;
 }
 
-//ordinamento ingenuo di un vettore di int
-int ordinamento_ingenuo(int v[], int len)
+//funzione per scambiare 2 variabili float
+void scambia_float(float &a, float &b)
 {
+	float box = a;
+	a = b;
+	b = box;
+}
+
+//creazione di un vettore con elementi random int
+void riempi_vettore_random_int(int v[], int len, int min, int max)
+{
+	for (int i=0; i<len; i++)
+	{
+		v[i] = random_int(min,max);
+	}
+}
+
+//creazione di un vettore con elementi random float
+void riempi_vettore_random_float(float v[], int len, float min, float max)
+{
+	for (int i=0; i<len; i++)
+	{
+		v[i] = random_float(min, max);
+	}
+}
+
+//stampa il vettore di int
+void stampa_vettore_int(int v[], int len)
+{
+	cout << "\n" << "Il tuo vetore INT: [";
+	for (int i=0; i<len; i++)
+	{
+		cout << " " << v[i] << ",";
+	}
+	cout << "]" << endl;
+}
+
+//stampa il vettore di float
+void stampa_vettore_float(float v[], int len)
+{
+	cout << "\n" << "Il tuo vetore Float: [";
+	for (int i=0; i<len; i++)
+	{
+		cout << " " << v[i] << ",";
+	}
+	cout << "]" << endl;
+}
+
+//ordinamento ingenuo di un vettore di int
+void ordinamento_ingenuo_int(int v[], int len)
+{
+	cout << "\nOrdino ingenuamente il vettore..." << endl;
 	for (int i=0; i<len-1; i++)
 	{
 		for (int j=i+1; j<len; j++)
 		{
 			if (v[i]>v[j])
-				scambia(v[i], v[j]);
+				scambia_int(v[i], v[j]);
 		}
 	}
-	
-	cout << "vettore ordinato: " << endl;
-	for (int i=0; i<len; i++)
-	{
-		cout << v[i] << " / ";
-	}
-	cout << endl;
 }
 
-//bubble sort di ordinamento vettore di int
-int bubbleSort(int v[], int len) 
-{ 
-   int i, j; 
-   bool ordinato; 
-   for (i = 0; i < len-1; i++) 
-   { 
-     ordinato = false; 
-     for (j = 0; j < len-i-1; j++) 
-     { 
-        if (v[j] > v[j+1]) 
-        { 
-           scambia(v[j], v[j+1]); 
-           ordinato = true; 
-        } 
-     } 
-  
-     // IF no two elements were swapped by inner loop, then break 
-     if (ordinato == false) 
-        break; 
-   } 
-   
-   cout << "vettore ordinato: " << endl;
-	for (int i=0; i<len; i++)
+//ordinamento ingenuo di un vettore di float
+void ordinamento_ingenuo_float(float v[], int len)
+{
+	cout << "\nOrdino ingenuamente il vettore..." << endl;
+	for (int i=0; i<len-1; i++)
 	{
-		cout << v[i] << " / ";
+		for (int j=i+1; j<len; j++)
+		{
+			if (v[i]>v[j])
+				scambia_float(v[i], v[j]);
+		}
 	}
-	cout << endl;
 }
 
-//Ordinamento per bubbleSort con SENTINELLA
-int bubbleSort_sentinella(int v[], int len) 
+//Ordinamento per bubbleSort con SENTINELLA di int
+void bubbleSort_int(int v[], int len) 
 { 
-   int i; 
+   cout << "\nOrdino il vettore con Bubble Sort..." << endl;
    bool ordinato; 
    do { 
      ordinato = false; 
-     for (i = 0; i < len-1; i++) 
+     for (int i = 0; i < len-1; i++) 
      { 
         if (v[i] > v[i+1]) 
         { 
-           scambia(v[i], v[i+1]); 
+           scambia_int(v[i], v[i+1]); 
            ordinato = true; 
         } 
      } 
   
    }while(ordinato==true);
-   
-   	cout << "vettore ordinato: " << endl;
-	for (int i=0; i<len; i++)
-	{
-		cout << v[i] << " / ";
-	}
-	cout << endl;
 }
 
-// ricerca elemento all'interno di un vettore
-bool ricerca_binaria(int v[], int elemento, int len)
+//Ordinamento per bubbleSort con SENTINELLA di float
+void bubbleSort_float(float v[], int len) 
+{ 
+   cout << "\nOrdino il vettore con Bubble Sort..." << endl;
+   bool ordinato; 
+   do { 
+     ordinato = false; 
+     for (int i = 0; i < len-1; i++) 
+     { 
+        if (v[i] > v[i+1]) 
+        { 
+           scambia_float(v[i], v[i+1]); 
+           ordinato = true; 
+        } 
+     } 
+  
+   }while(ordinato==true);
+}
+
+// ricerca elemento all'interno di un vettore di int
+bool ricerca_binaria_int(int v[], int elemento, int len)
 {
 	int count=1, i=0, f=len-1, m;
 	while(i<=f)
@@ -117,5 +159,27 @@ bool ricerca_binaria(int v[], int elemento, int len)
 	
 	cout << "Il numero " << elemento << " NON viene trovato nel vettore" << endl;
 	return false;
+}
+
+// ricerca elemento all'interno di un vettore di float
+bool ricerca_binaria_float(float v[], float elemento, int len)
+{
+	int count=1, i=0, f=len-1, m;
+	while(i<=f)
+	{
+		m = (i+f)/2;
+		if (v[m] > elemento)
+			f = m - 1;
+		else if (v[m] < elemento)
+			i = m + 1;
+		else if (v[m] == elemento)
+		{
+			cout << "Il numero " << elemento << " trovato in " << count << " passaggi" << endl;
+			return true;
+		}
+		count += 1;
+	}
 	
+	cout << "Il numero " << elemento << " NON viene trovato nel vettore" << endl;
+	return false;
 }
--- libreria_personale/myprogram.cpp
+++ libreria_personale/myprogram.cpp
@@ -6,5 +6,46 @@ using namespace std;
 
 int main()
 {
-	cout << raddoppia(3) << endl;
+	int len;
+	cout << "Scrivere la lunghezza dei vettori: ";
+	cin >> len; cout << endl;
+	
+	srand(time(NULL));
+	
+	int min, max, Vint[len], findint;
+	float minf, maxf, Vfloat[len], findfloat;
+	
+	cout << "Scrivere il numero minimo del vettore INT: ";
+	cin >> min; cout << endl;
+	cout << "Scrivere il numero massimo del vettore INT: ";
+	cin >> max; cout << endl;
+	cout << "Scrivere il numero minimo del vettore FLOAT: ";
+	cin >> minf; cout << endl;
+	cout << "Scrivere il numero massimo del vettore FLOAT: ";
+	cin >> maxf; cout << endl;
+	
+	riempi_vettore_random_int(Vint, len, min, max);
+	riempi_vettore_random_float(Vfloat, len, minf*10, maxf*10);
+	
+	stampa_vettore_int(Vint, len);
+	stampa_vettore_float(Vfloat, len);
+	
+	//PER USARE UN ORDINAMENTO BISOGNA COMMENTARE QUELLO INUTILIZZATO
+	
+	ordinamento_ingenuo_int(Vint, len);
+	ordinamento_ingenuo_float(Vfloat, len);
+	
+	bubbleSort_int(Vint, len);
+	bubbleSort_float(Vfloat, len);
+	
+	stampa_vettore_int(Vint, len);
+	stampa_vettore_float(Vfloat, len);
+	
+	cout << "\nScrivere il numero da cercare nel vettore INT: ";
+	cin >> findint; cout << endl;
+	cout << "Scrivere il numero da cercare nel vettore FLOAT: ";
+	cin >> findfloat; cout << endl;
+	
+	ricerca_binaria_int(Vint, findint, len);
+	ricerca_binaria_float(Vfloat, findfloat, len);
 }
